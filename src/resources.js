9c6ccc5dd21e9bb2767a964bc9801b04e14e394c
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-05-15 01:25:03 +0900

    support English. fix typos.

--- src/resources.js
+++ src/resources.js
@@ -23,7 +23,7 @@ export const getResources = (lang) => {
             title: resolve(() => "概略", () => "Digest"),
             text: resolve(
                 () => "ウェブアプリケーション、デスクトップアプリケーション、スマートフォンアプリケーションなどを開発するプログラマーです。",
-                () => "A software engineer who develops web applications, desktop application, smart phone applications and so on."
+                () => "A software engineer who develops web applications, desktop applications, smart phone applications and so on."
             ),
         },
         education: {
@@ -42,41 +42,52 @@ export const getResources = (lang) => {
             employments: [
                 {
                     company: resolve(() => "株式会社エイ・シー・ティ", () => "A.C.T. Inc."),
-                    type: resolve(() => "契約社員", () => ""),
+                    type: resolve(() => "契約社員", () => "Contracted employee"),
                     start: resolve(() => "2016年3月", () => "March 2016"),
                     end: resolve(() => "2017年5月", () => "May 2017"),
                     works: [
-                        work(resolve(() => "Androidアプリケーション（Java 7）の開発を行なった。", () => "")),
-                        work(resolve(() => "NET MVC（C# 6）によるウェブサイトの開発を行なった。SQL Server を扱った。", () => "")),
                         work(resolve(
-                            () => "一部プロジェクトでGit，Vagrantの利用を提案し、導入が行なわれた。"
+                            () => "Androidアプリケーション（Java 7）の開発を行なった。",
+                            () => "Exercised to develop an Android application (Java 7).")),
+                        work(resolve(
+                            () => ".NET MVC（C# 6）によるウェブサイトの開発を行なった。SQL Server を扱った。",
+                            () => "Exercised to develop a web site with .NET MVC (C# 6). Used SQL Server.")),
+                        work(resolve(
+                            () => "一部プロジェクトでGit，Vagrantの使用を提案し、導入が行なわれた。"
                                 + "導入にあたってGitサーバーの構築を担当した。",
-                            () => ""
-                        )),
+                            () => "Suggested to apply Git and Vagrant for certain projects and introducing of them are carried out. "
+                                + "Took charge of constructing Git server on introducing.")),
                     ]
                 },
                 {
                     company: resolve(() => "ＦＫＣ株式会社", () => "FKC Inc."),
-                    type: resolve(() => "正社員", () => ""),
+                    type: resolve(() => "正社員", () => " Regular employee"),
                     start: resolve(() => "2015年07月", () => "July 2015"),
                     end: resolve(() => "2016年03月", () => "March 2016"),
                     works: [
-                        work(resolve(() => ".NET（C# 3）による Windows アプリケーションの開発を行なった。", () => "")),
-                        work(resolve(() => "PHP 5.2（フルスクラッチ）によるウェブサイトの開発を行なった。SQL Server を扱った。", () => "")),
+                        work(resolve(
+                            () => ".NET（C# 3）による Windows アプリケーションの開発を行なった。",
+                            () => "Excercised to develop a Windows application with .NET (C# 3).")),
+                        work(resolve(
+                            () => "PHP 5.2（フルスクラッチ）によるウェブサイトの開発を行なった。SQL Server を扱った。",
+                            () => "Excercised to develop a web site with PHP 5.2 from scratch. Used SQL Server.")),
                     ],
                 },
                 {
                     company: resolve(() => "株式会社ＲＭＡ", () => "RMA Inc."),
-                    type: resolve(() => "派遣社員・正社員", () => ""),
+                    type: resolve(() => "派遣社員・正社員", () => "Dispatched employee / regular employee"),
                     start: resolve(() => "2014年10月" , () => "October 2014"),
                     end: resolve(() => "2015年06月", () => "June 2015"),
                     works: [
-                        work(resolve(() => "CakePHP（PHP 5.4）によるウェブサイトの開発（バックエンド、フロントエンド）を行なった。MySQLを扱った。", () => "")),
-                        work(resolve(() => "NET（C# 3）による Windows アプリケーションの開発を行なった。SQL Server を扱った。", () => "")),
+                        work(resolve(
+                            () => "CakePHP（PHP 5.4）によるウェブサイトの開発（バックエンド、フロントエンド）を行なった。MySQLを扱った。",
+                            () => "Excercised to develop a web site (backend and frontend) with CakePHP (PHP 5.4). Used MySQL.")),
+                        work(resolve(
+                            () => ".NET（C# 3）による Windows アプリケーションの開発を行なった。SQL Server を扱った。",
+                            () => "Excercised to develop a Windows application with .NET (C# 3). Used SQL Server.")),
                         work(resolve(
                             () => "Dropwizard（Java 7）による REST サービスおよびフロントエンドの開発を行なった。DynamoDB を扱った。",
-                            () => ""
-                        )),
+                            () => "Excercised to develop a REST service with Dropwizard (Java 7) and frontend site. Used DynamoDB.")),
                     ],
                 }
             ],
@@ -85,15 +96,21 @@ export const getResources = (lang) => {
         privateActivities: {
             title: resolve(() => "業務外の活動", () => "Private Activities"),
             items: [
-                resolve(() => "大学院生へのオブジェクト指向プログラミングの指導", () => ""),
-                resolve(() => "Kotlin による、Androidアプリケーションの開発", () => ""),
-                resolve(() => "React 16, Angular 4 の学習", () => ""),
-                resolve(() => "Django、Ansible によるウェブサイトの開発", () => ""),
+                resolve(() => "大学院生へのオブジェクト指向プログラミングの指導", () => "Teaching object-oriented programming to a graduate student"),
+                resolve(() => "Kotlin による、Androidアプリケーションの開発", () => "Development of an Android application with Kotlin"),
+                resolve(() => "React 16, Angular 4 の学習", () => "Learning React 16 and Angular 4"),
+                resolve(() => "Django、Ansible によるウェブサイトの開発", () => "Development of a web site with Django and Ansible"),
                 {
-                    description: resolve(() => "Docker によるマイクロサービスで構成されたウェブアプリケーションの開発", () => ""),
+                    description: resolve(
+                        () => "Docker によるマイクロサービスで構成されたウェブアプリケーションの開発",
+                        () => "Development a web application composed of microservices with Docker"),
                     annotations: [
-                        resolve(() => "Scala による REST サービス、Vue.js によるフロントエンド（nginx）、MongoDB で構成している。", () => ""),
-                        resolve(() =>"FunSpec、Travis CI、webpack、TypeScript を利用している。", () => ""),
+                        resolve(
+                            () => "Scala による REST サービス、Vue.js によるフロントエンド（nginx）、MongoDB で構成している。",
+                            () => "composed of REST service with Scala, frontend with Vue.js (nginx), and MongoDB."),
+                        resolve(
+                            () =>"FunSpec、Travis CI、webpack、TypeScript を使用している。",
+                            () => "using FunSpec, Travis CI, webpack and TypeScript."),
                     ]
                 }
             ].map(activity),
@@ -101,13 +118,27 @@ export const getResources = (lang) => {
         skills: {
             title: resolve(() => "その他のスキル", () => "Other Skills"),
             items: [
-                resolve(() => "Java 8 でのプログラミング（Stream などを利用する）ができる。", () => ""),
-                resolve(() => "Perl、Python、Haskellでのプログラミングが少しできる。", () => ""),
-                resolve(() => "SSH で Linux の基本的な操作ができる。", () => ""),
-                resolve(() => "関数型プログラミングの基礎を理解している。", () => ""),
-                resolve(() => "アルゴリズムとデータ構造の基礎を理解している。", () => ""),
-                resolve(() => "リレーショナルデータベースの基礎を理解し、基本的なパフォーマンスチューニングができる。", () => ""),
-                resolve(() => "ウェブアプリケーションの基本的なセキュリティを理解している。", () => ""),
+                resolve(
+                    () => "Java 8 でのプログラミング（Stream などを使用する）ができる。",
+                    () => "Programming with Java 8 (using Stream etc.)."),
+                resolve(
+                    () => "Perl、Python、Haskellでのプログラミングが少しできる。",
+                    () => "Programming with Perl, Python and Haskell a little."),
+                resolve(
+                    () => "SSH で Linux の基本的な操作ができる。",
+                    () => "Basic operation of Linux through SSH."),
+                resolve(
+                    () => "関数型プログラミングの基礎を理解している。",
+                    () => "Understand the basics of functional programming."),
+                resolve(
+                    () => "アルゴリズムとデータ構造の基礎を理解している。",
+                    () => "Understand the basics of algorithms and data structures."),
+                resolve(
+                    () => "リレーショナルデータベースの基礎を理解し、基本的なパフォーマンスチューニングができる。",
+                    () => "Understand the basics of relational database and be able to carry out basic performance tuning for them."),
+                resolve(
+                    () => "ウェブアプリケーションの基本的なセキュリティを理解している。",
+                    () => "Understand about basic securty of web applications."),
             ].map(item => ({ description: item })),
         },
         myAccounts: {
@@ -124,16 +155,18 @@ export const getResources = (lang) => {
             format: resolve(() => e => e.date + "　" + e.title, () => e => e.date + " " + e.title)
         },
         favoriteBooks: {
-            title: resolve(() => "お気に入りの書籍", () => "Favorite books"),
+            title: resolve(() => "お気に入りの書籍", () => "Favorite Books"),
             items: [
-                resolve(() => "『すごいHaskell たのしく学ぼう！』", () => ""),
-                resolve(() => "『Javaによる関数型プログラミング』", () => ""),
-                resolve(() => "『リーダブルコード』", () => ""),
-                resolve(() => "『ThoughtWorksアンソロジー』", () => ""),
-                resolve(() => "『SQLアンチパターン』", () => ""),
-                resolve(() => "『アルゴリズムイントロダクション 第1巻』", () => ""),
-                resolve(() => "『体系的に学ぶ 安全なWebアプリケーションの作り方』", () => ""),
-                resolve(() => "『型システム入門』", () => ""),
+                resolve(() => "『すごいHaskell たのしく学ぼう！』", () => "Learn You a Haskell for Great Good!"),
+                resolve(() => "『Javaによる関数型プログラミング』", () => "Functional Programming in Javar"),
+                resolve(() => "『リーダブルコード』", () => "The Art of Readable"),
+                resolve(() => "『ThoughtWorksアンソロジー』", () => "The ThoughtWorks Anthology"),
+                resolve(() => "『SQLアンチパターン』", () => "SQL Antipatterns"),
+                resolve(() => "『アルゴリズムイントロダクション 第1巻』", () => "Introduction to Algorithms"),
+                resolve(
+                    () => "『体系的に学ぶ 安全なWebアプリケーションの作り方』",
+                    () => "『体系的に学ぶ 安全なWebアプリケーションの作り方』(Japanese)"),
+                resolve(() => "『型システム入門』", () => "Types and Programming Languages"),
             ].map(item => ({ title: item })),
         },
     };
