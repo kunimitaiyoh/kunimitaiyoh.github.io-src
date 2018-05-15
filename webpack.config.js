c5c85fd9d2d6716c0410553c33de5f1883084ab9
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-05-15 13:26:26 +0900

    enable minimization of bundled script.

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,20 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurifyCSSPlugin = require("purifycss-webpack");
+const glob = require("glob-all");
 
-module.exports = {
+const isProduction = process.argv.includes("-p");
+
+module.exports = (env, argv) => ({
     mode: "development",
     output: {
         path: path.resolve(__dirname, "dist"),
         publicPath: "/",
     },
-    devtool: "inline-source-map",
+    devtool: isProduction ? false : "source-map",
     devServer: {
         historyApiFallback: true,
         contentBase: "dist",
@@ -17,31 +23,30 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            use: [{
-                loader: "babel-loader",
-                options: {
-                    presets: ["react"],
-                    plugins: [
-                        "transform-react-jsx",
-                        "jsx-control-statements"
-                    ]
-                }
-            }]
-        },
-        {
             test: /\.html$/,
             exclude: /node_modules/,
             loader: "html-loader"
         },
         {
             test: /\.css$/,
-            use: ["style-loader", "css-loader"],
+            use: [MiniCssExtractPlugin.loader, "css-loader?minimize"],
         },
         {
             test: /\.(jpe?g|png|svg|ttf|woff|woff2|eot)$/,
             loaders: "file-loader?name=[name].[ext]"
+        },
+        {
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: "babel-loader",
+                options: {
+                    presets: ["react"],
+                    plugins: [
+                        "jsx-control-statements"
+                    ]
+                }
+            }]
         }]
     },
     resolve: {
@@ -57,5 +62,27 @@ module.exports = {
         new ScriptExtHtmlWebpackPlugin({
             defer: [/.jsx?$/],
         }),
-    ]
-};
+    ].concat(isProduction ? [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        }),
+        new PurifyCSSPlugin({
+            minimize: true,
+            paths: glob.sync([
+                path.join(__dirname, 'public/**/*.html'),
+                path.join(__dirname, 'src/**/*.jsx'),
+                path.join(__dirname, 'src/**/*.css'),
+            ]),
+        }),
+        new UglifyJsPlugin({
+            sourceMap: false,
+            uglifyOptions: {
+                compress: true,
+                output: {
+                    comments: false,
+                },
+            },
+        }),
+    ] : [])
+});
