dcd60f5e0777cf98271aa9676c929caa64804233
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2019-05-13 03:14:12 +0900

    (WIP) working with RouteComponentProps in application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import * as React from "react"
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, RouteComponentProps } from "react-router-dom";
 import Profile from "@/views/Profile";
 import { Resources } from "./resources";
 
 export function App(props: AppProps): JSX.Element {
+
+  function render(component: (props: AppProps) => JSX.Element): (route: RouteComponentProps) => JSX.Element {
+    return (route) => {
+      // TODO: build AppProps using route here.
+
+      return component(null as any);
+    };
+  }
+
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" render={ () => <Profile { ...props } /> } ></Route>
+        <Route exact path="/" render={ render(props => <Profile { ...props } />) } ></Route>
       </Switch>
     </BrowserRouter>
   );
