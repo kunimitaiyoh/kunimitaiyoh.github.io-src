6eee2aacf298fa6ec6aa97e7eb32ebd45795ebb2
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2019-06-19 20:08:51 +0900

    (WIP) introducing resources.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import * as React from "react"
 import { BrowserRouter, Route, Switch, RouteComponentProps } from "react-router-dom";
 import Profile from "@/views/Profile";
 import { Resources } from "./resources";
+import { getEnvironment, extractQueryParams } from "./util";
 
-export function App(props: AppProps): JSX.Element {
+export function App(props: PropsBase): JSX.Element {
 
   function render(component: (props: AppProps) => JSX.Element): (route: RouteComponentProps) => JSX.Element {
     return (route) => {
-      // TODO: build AppProps using route here.
+      const queryParams = extractQueryParams(route.location.search);
+      const env = getEnvironment(queryParams, window);
 
-      return component(null as any);
+      return component({ ...props });
     };
   }
 
@@ -22,9 +24,12 @@ export function App(props: AppProps): JSX.Element {
   );
 }
 
-export interface AppProps {
+export interface PropsBase {
   buildDate: any;
   resources: Resources;
   environment: any;
   age: any;
+}
+
+export interface AppProps extends PropsBase {
 }
--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { BUILD_DATE } from "@/config";
 
   const buildDate = Instant.parse(BUILD_DATE);
   const age = util.calculateAge(1990, 12, 21, new Date());
-  const queryParams = util.extractQueryParams(window);
+  const queryParams = util.extractQueryParams(window.location.search);
   const environment = util.getEnvironment(queryParams, window);
   const resources = await res.getResources(environment.language);
   const props = { buildDate, resources, environment, age };
